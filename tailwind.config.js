cbb876985b23c98507dcf534956e09c1c1435e8e
michel-lopez-franco <michel.lopez@academicos.udg.mx>
2025-11-07 02:31:06 -0600

    se agrego tailwind local.

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./index.html", "./*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/Tutorial-Visual-de-Codificadores-de-ML/',
       server: {
         port: 3000,
         host: '0.0.0.0',
